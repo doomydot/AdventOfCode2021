b05e9488e635b2c34b3e4bd8e720756d1a534e46
Olof Hagström <olof.hagstrom@gmail.com>
2021-12-02 23:36:41 +0100

    removed obsolete projects

--- AdventOfCode/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-int count = -1; int last = 0;
-foreach (string line in System.IO.File.ReadLines("input1.txt"))
-{
-	if (int.Parse(line) > last) count++;
-	last = int.Parse(line);
-}
-Console.Write(count);
