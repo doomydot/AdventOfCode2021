97d14ea45c9882ee21814d8fcc4dcc6da9619ddb
doomydot <olof.hagstrom@gmail.com>
2021-12-03 11:00:28 +0100

    day 3 part 1 solution

---
+++ Day 3-1/Program.cs
@@ -0,0 +1,39 @@
+﻿List<string> input = new List<string>();
+foreach (string line in File.ReadLines("input.txt"))
+    input.Add(line);
+
+string gamma = "";
+string epsilon = "";
+List<int> gammaRaw = new List<int>();
+foreach (char meme in input[0])
+    gammaRaw.Add(0);
+
+foreach (string line in input)
+{
+    int count = 0;
+    string[] lineSplit = line.Split();
+    foreach (char c in lineSplit[count])
+    {
+        gammaRaw[count] = int.Parse(gammaRaw[count].ToString()) + int.Parse(c.ToString());
+        count++;
+    } 
+}
+
+foreach (int g in gammaRaw)
+{
+    if (g > 499)
+    {
+        gamma += 1;
+        epsilon += 0;
+    }
+        
+    else if (g < 500)
+    {
+        gamma += 0;
+        epsilon += 1;
+    }   
+}
+
+Console.Write(Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2));
+    
+
